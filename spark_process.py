e92aa91ba6d552aaafc8c1256688bd151f642613
Rajesh P <rajesh.pethe@gmail.com>
2018-12-25 13:04:08 +0530

    Load CSV Sentiment140 data and convert it to DataFrame.

--- spark_process.py
+++ spark_process.py
@@ -1,30 +1,73 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import Row, SQLContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType
+
 import sys
 import requests
 
-conf = SparkConf().setAppName('TwitterStream')
+from config import SENTIMENT140_DATA
 
-sc = SparkContext(conf=conf)
+# Initialize the spark config
+conf = SparkConf().setAppName('TwitterStream').setMaster("local[*]")
+
+# Create the spark context
+sc = SparkContext.getOrCreate(conf=conf)
+# Suppress debug messages
 sc.setLogLevel("ERROR")
 
-ssc = StreamingContext(sc, 2)
 
-ssc.checkpoint("checkpoint_TwitterApp")
+def read_twitter_stream(sc):
+    ssc = StreamingContext(sc, 2)
 
-dataStream = ssc.socketTextStream("localhost", 9009)
+    ssc.checkpoint("checkpoint_TwitterApp")
 
-words = dataStream.flatMap(lambda line: print(line))
+    dataStream = ssc.socketTextStream("localhost", 9009)
 
-dataStream.pprint(num=2)
+    words = dataStream.flatMap(lambda line: print(line))
 
-# start the streaming computation
-ssc.start()
-# wait for the streaming to finish
-try:
-    ssc.awaitTermination()
-except KeyboardInterrupt:
-    print("STOPPING...!!!")
-    ssc.stop()
+    dataStream.pprint(num=2)
+
+    # start the streaming computation
+    ssc.start()
+    # wait for the streaming to finish
+    try:
+        ssc.awaitTermination()
+    except KeyboardInterrupt:
+        print("STOPPING...!!!")
+        ssc.stop()
+
+
+def loadSentiment140(sc, path):
+    # Create spark session for Dataset and DataFrame API
+    spark = SparkSession(sc)
+
+    '''
+        Another way to create SparkSession -
+
+        spark = SparkSession \
+            .builder \
+            .appName("Python Spark SQL basic example") \
+            .config("spark.some.config.option", "some-value") \
+            .getOrCreate()
+
+    '''
+
+    # Defined Dataframe columns for Sentiment140 data
+    cols = ["polarity", "id", "date", "query", "user", "status"]
+    # Defined schema
+    schema = [ StructField(fname, StringType(), True) for fname in cols ]
+    schema = StructType(schema)
+
+    # Load data from CSV and get DataFrame.
+    df = spark.read.csv(path, schema=schema)
+
+    # Drop unwanted columns
+    df = df.drop("id", "date", "query", "user")
+    df.show()
+
+
+loadSentiment140(sc, SENTIMENT140_DATA)
+sc.stop()
 
